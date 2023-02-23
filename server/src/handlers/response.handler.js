1203304415925526c23f78abc494486f6299a435
Toyly Myratgeldiyev <dayow93@inbox.ru>
2023-02-23 20:03:45 +0300

    Create handlers

--- server/src/handlers/response.handler.js
+++ server/src/handlers/response.handler.js
@@ -14,7 +14,26 @@ const badrequest = (res,message)=>responseWithData(res,400,{
     message
 })
 
-const ok = (res,message) => responseWithData(res, 200,{
-    status:200,
-    message
+const ok = (res,message) => responseWithData(res, 200,data)
+
+const created = (res,data) => responseWithData(res,201,data)
+
+const unauthorize = (res) => responseWithData(res,401,{
+    status: 401,
+    message: "Unathorized"
 })
+
+
+const notfound = (res) => responseWithData(res,404,{
+    status: 404,
+    message: "Resoursed not found"
+})
+
+export default {
+    error,
+    badrequest,
+    ok,
+    created,
+    unauthorize,
+    notfound
+}
